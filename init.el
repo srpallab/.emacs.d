7eb8032e3eb050a1e63edaf0539b2d68683eeeda
Shafiqur Rahman <pallab.alif@gmail.com>
2019-10-12 14:38:07 +0600

    ADD: Elpy Python Mode, company and iedit.

--- init.el
+++ init.el
@@ -21,9 +21,13 @@
  '(custom-safe-themes
    (quote
     ("a7051d761a713aaf5b893c90eaba27463c791cd75d7257d3a8e66b0c8c346e77" default)))
+ '(elpy-rpc-python-command "python3")
+ '(elpy-rpc-virtualenv-path "/usr/local/bin/virtualenv")
  '(package-selected-packages
    (quote
-    (which-key yasnippet projectile dashboard magit org-bullets doom-themes doom-theme spaceline doom-modeline zenburn-theme use-package))))
+    (iedit flycheck elpy which-key yasnippet projectile dashboard magit org-bullets doom-themes doom-theme spaceline doom-modeline zenburn-theme use-package)))
+ '(python-shell-completion-native-disabled-interpreters (quote ("pypy")))
+ '(python-shell-interpreter "python3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
